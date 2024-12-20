5fc710efe0edfa002425bbfbc50422ac9c63c9a8
kat <28567881+hk21702@users.noreply.github.com>
2024-12-20 16:08:36 -0500

    Test consent CLI flags

--- cmd/insights/commands/consent_test.go
+++ cmd/insights/commands/consent_test.go
@@ -1,1 +1,26 @@
 package commands
+
+import (
+	"testing"
+
+	"github.com/ubuntu/ubuntu-insights/internal/testutils"
+)
+
+func TestConsentFlags(t *testing.T) {
+	testCases := []testutils.CmdTestCase{
+		{
+			Name:    "source",
+			Short:   "s",
+			BaseCmd: consentCmd,
+		}, {
+			Name:    "consent-state",
+			Short:   "c",
+			BaseCmd: consentCmd,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			testutils.FlagTestHelper(t, tc)
+		})
+	}
+}
