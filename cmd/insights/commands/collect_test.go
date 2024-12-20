0fc8a1e305c76a5d5938873d4964cfc7adc8a9bc
kat <28567881+hk21702@users.noreply.github.com>
2024-12-20 14:15:43 -0500

    Add collect command flag tests

---
+++ cmd/insights/commands/collect_test.go
@@ -0,0 +1,54 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlagsExist(t *testing.T) {
+	testCases := []struct {
+		name     string
+		short    string
+		required bool
+		dirname  bool
+	}{{
+		name:     "source",
+		short:    "s",
+		required: true,
+	}, {
+		name:    "dir",
+		dirname: true,
+	}, {
+		name:  "period",
+		short: "p",
+	}, {
+		name:  "force",
+		short: "f",
+	}, {
+		name:  "dry-run",
+		short: "d",
+	},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			flag := collectCmd.Flags().Lookup(tc.name)
+			assert.NotNil(t, flag)
+			assert.Equal(t, tc.short, flag.Shorthand)
+
+			if tc.required {
+				assert.Equal(t, "true", flag.Annotations[cobra.BashCompOneRequiredFlag][0])
+			} else {
+				assert.Nil(t, flag.Annotations[cobra.BashCompOneRequiredFlag])
+			}
+
+			if tc.dirname {
+				assert.Equal(t, []string{}, flag.Annotations[cobra.BashCompSubdirsInDir])
+			} else {
+				assert.Nil(t, flag.Annotations[cobra.BashCompSubdirsInDir])
+			}
+		})
+	}
+}
