cb7791bf9b1c45d5903082534e03c2e7b0be6a50
kat <28567881+hk21702@users.noreply.github.com>
2025-01-08 22:21:04 -0500

    Change collect metrics path flag to be an argument, update collect argument behavior. Disable default cobra error printing. Update CLI flag descriptions

--- cmd/insights/commands/collect.go
+++ cmd/insights/commands/collect.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -26,13 +28,39 @@ func installCollectCmd(app *App) error {
 	app.collectConfig = defaultCollectConfig
 
 	collectCmd := &cobra.Command{
-		Use:   "collect [SOURCE](required argument)",
+		Use:   "collect [SOURCE] [SOURCE-METRICS-PATH](required if source provided)",
 		Short: "Collect system information",
-		Long:  "Collect system information and metrics and store it locally",
-		Args:  cobra.ExactArgs(1),
+		Long: `Collect system information and metrics and store it locally.
+		If SOURCE is not provided, then it is the source is assumed to be the currently detected platform. Additionally, there should be no SOURCE-METRICS-PATH provided.
+		If SOURCE is provided, then the SOURCE-METRICS-PATH should be provided as well.`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if err := cobra.MaximumNArgs(2)(cmd, args); err != nil {
+				return err
+			}
+
+			if len(args) != 0 {
+				if err := cobra.MatchAll(cobra.OnlyValidArgs, cobra.ExactArgs(2))(cmd, args); err != nil {
+					return fmt.Errorf("accepts no args, or exactly 2 args, received 1")
+				}
+
+				fileInfo, err := os.Stat(args[1])
+				if err != nil {
+					return fmt.Errorf("the second argument, SOURCE-METRICS-PATH, should be a valid JSON file. Error: %s", err.Error())
+				}
+
+				if fileInfo.IsDir() {
+					return fmt.Errorf("the second argument, SOURCE-METRICS-PATH, should be a valid JSON file, not a directory.")
+				}
+			}
+
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// Set Sources to Args
-			app.collectConfig.source = args[0]
+			if len(args) == 2 {
+				app.collectConfig.source = args[0]
+				app.collectConfig.extraMetrics = args[1]
+			}
 
 			slog.Info("Running collect command")
 
@@ -40,15 +68,9 @@ func installCollectCmd(app *App) error {
 		},
 	}
 
-	collectCmd.Flags().UintVarP(&app.collectConfig.period, "period", "p", 1, "The minimum period between 2 collection periods for validation purposes in seconds")
-	collectCmd.Flags().BoolVarP(&app.collectConfig.force, "force", "f", false, "Force a collection, override the report if there are any conflicts. (Doesn't ignore consent)")
-	collectCmd.Flags().BoolVarP(&app.collectConfig.dryRun, "dry-run", "d", false, "Perform a dry-run where a report is collected, but not written to disk")
-	collectCmd.Flags().StringVarP(&app.collectConfig.extraMetrics, "extra-metrics", "e", "", "Path to JSON file to append extra metrics from")
-
-	if err := collectCmd.MarkFlagFilename("extra-metrics", "json"); err != nil {
-		slog.Error("An error occurred while initializing the collect command.", "error", err.Error())
-		return err
-	}
+	collectCmd.Flags().UintVarP(&app.collectConfig.period, "period", "p", 1, "the minimum period between 2 collection periods for validation purposes in seconds")
+	collectCmd.Flags().BoolVarP(&app.collectConfig.force, "force", "f", false, "force a collection, override the report if there are any conflicts (doesn't ignore consent)")
+	collectCmd.Flags().BoolVarP(&app.collectConfig.dryRun, "dry-run", "d", false, "perform a dry-run where a report is collected, but not written to disk")
 
 	app.rootCmd.AddCommand(collectCmd)
 	return nil
