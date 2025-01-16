6160af74236ee20129ad9998abb93114460ff29a
kat <28567881+hk21702@users.noreply.github.com>
2025-01-16 17:02:14 -0500

    Refactor consent manager to simplify functions

--- internal/consent/consent.go
+++ internal/consent/consent.go
@@ -19,18 +19,6 @@ type Manager struct {
 	path string
 }
 
-// States is a struct that represents the consent states for a list of sources and the global consent state.
-type States struct {
-	SourceStates map[string]consentStateResult
-	GlobalState  consentStateResult
-}
-
-type consentStateResult struct {
-	Source  string // the source for the consent state
-	State   bool   // the consent state for the source
-	ReadErr bool   // true if there was an error reading the consent file
-}
-
 // consentFile is a struct that represents a consent file.
 type consentFile struct {
 	ConsentState bool `toml:"consent_state"`
@@ -42,59 +30,18 @@ func New(path string) *Manager {
 	return &Manager{path: path}
 }
 
-// GetConsentStates gets the consent state for the given sources and the global consent state.
-// If any of the sources do not have a consent file, it will be considered as a false state.
-// If a specified source does not have a consent file, it will not be included in the returned ConsentStates struct.
-// TODO: Simplify to single source, update spec
-func (cm Manager) GetConsentStates(sources []string) (consentStates States, err error) {
-	consentStates = States{SourceStates: make(map[string]consentStateResult)}
-
-	sourceFiles, globalFile, err := getMatchingConsentFiles(sources, cm.path)
+// GetConsentState gets the consent state for the given source.
+// If the source do not have a consent file, it will be considered as a false state.
+// If the source is an empty string, then the global consent state will be returned.
+// If the target consent file does not exist, it will not be created.
+func (cm Manager) GetConsentState(source string) (bool, error) {
+	sourceConsent, err := readConsentFile(cm.getConsentFile(source))
 	if err != nil {
-		slog.Error("Error getting consent files", "error", err)
-		return States{}, err
+		slog.Error("Error reading source consent file", "source", source, "error", err)
+		return false, err
 	}
 
-	results := make(chan consentStateResult, len(sourceFiles))
-	defer close(results)
-
-	// Global consent file
-	if globalFile != "" {
-		globalResult := make(chan consentStateResult, 1)
-		defer close(globalResult)
-
-		go func() {
-			globalConsent, err := readConsentFile(globalFile)
-			if err != nil {
-				slog.Error("Error reading global consent file", "file", globalFile, "error", err)
-				globalResult <- consentStateResult{Source: "global", State: false, ReadErr: true}
-				return
-			}
-			globalResult <- consentStateResult{Source: "global", State: globalConsent.ConsentState, ReadErr: false}
-		}()
-
-		consentStates.GlobalState = <-globalResult
-	}
-
-	// Goroutine to read the consent files for each source, excluding the global consent file.
-	for source, filePath := range sourceFiles {
-		go func(source, filePath string) {
-			consent, err := readConsentFile(filePath)
-			if err != nil {
-				slog.Error("Error reading consent file", "source", source, "error", err)
-				results <- consentStateResult{Source: source, State: false, ReadErr: true}
-				return
-			}
-			results <- consentStateResult{Source: source, State: consent.ConsentState, ReadErr: false}
-		}(source, filePath)
-	}
-
-	for range sourceFiles {
-		res := <-results
-		consentStates.SourceStates[res.Source] = res
-	}
-
-	return consentStates, nil
+	return sourceConsent.ConsentState, nil
 }
 
 var consentSourceFilePattern = `%s` + constants.ConsentSourceBaseSeparator + constants.GlobalFileName
@@ -105,26 +52,29 @@ var consentSourceFilePattern = `%s` + constants.ConsentSourceBaseSeparator + con
 func (cm Manager) SetConsentState(source string, state bool) (err error) {
 	defer decorate.OnError(&err, "could not set consent state")
 
+	consent := consentFile{ConsentState: state}
+	return consent.write(cm.getConsentFile(source))
+}
+
+// getConsentFile returns the expected path to the consent file for the given source.
+// If source is blank, it returns the path to the global consent file.
+// It does not check if the file exists, or if it is valid.
+func (cm Manager) getConsentFile(source string) string {
 	p := filepath.Join(cm.path, constants.GlobalFileName)
 	if source != "" {
 		p = filepath.Join(cm.path, fmt.Sprintf(consentSourceFilePattern, source))
 	}
 
-	consent := consentFile{ConsentState: state}
-	return consent.write(p)
+	return p
 }
 
-// getMatchingConsentFiles returns a map of all paths to consent files matching the given sources and a path
-// to the global consent file.
-// If sources is empty, all consent files in the folder will be returned.
-// If a source does not have a consent file, it will be represented as an empty string
-// Does not traverse subdirectories.
-func getMatchingConsentFiles(sources []string, folderPath string) (sourceFiles map[string]string, globalFile string, err error) {
-	sourceFiles = make(map[string]string)
+// getSourceConsentFiles returns a map of all paths to validly named consent files in the folder, other than the global file.
+func (cm Manager) getConsentFiles() (map[string]string, error) {
+	sourceFiles := make(map[string]string)
 
-	entries, err := os.ReadDir(folderPath)
+	entries, err := os.ReadDir(cm.path)
 	if err != nil {
-		return sourceFiles, globalFile, err
+		return sourceFiles, err
 	}
 
 	for _, entry := range entries {
@@ -132,40 +82,20 @@ func getMatchingConsentFiles(sources []string, folderPath string) (sourceFiles m
 			continue
 		}
 
-		// Global file
-		if entry.Name() == constants.GlobalFileName {
-			globalFile = filepath.Join(folderPath, entry.Name())
-			slog.Debug("Found global consent file", "file", globalFile)
+		// Source file
+		if !strings.HasSuffix(entry.Name(), constants.ConsentSourceBaseSeparator+constants.GlobalFileName) {
 			continue
 		}
-
-		if len(sources) == 0 {
-			// Source file
-			if !strings.HasSuffix(entry.Name(), constants.ConsentSourceBaseSeparator+constants.GlobalFileName) {
-				continue
-			}
-			source := strings.TrimSuffix(entry.Name(), constants.ConsentSourceBaseSeparator+constants.GlobalFileName)
-			sourceFiles[source] = filepath.Join(folderPath, entry.Name())
-			slog.Debug("Found source consent file", "file", sourceFiles[source])
-			continue
-		}
-
-		for _, source := range sources {
-			if entry.Name() == fmt.Sprintf(consentSourceFilePattern, source) {
-				sourceFiles[source] = filepath.Join(folderPath, entry.Name())
-				slog.Debug("Found matching source consent file", "file", sourceFiles[source])
-				break
-			}
-		}
+		source := strings.TrimSuffix(entry.Name(), constants.ConsentSourceBaseSeparator+constants.GlobalFileName)
+		sourceFiles[source] = filepath.Join(cm.path, entry.Name())
+		slog.Debug("Found source consent file", "file", sourceFiles[source])
 	}
-	return sourceFiles, globalFile, err
+
+	return sourceFiles, nil
 }
 
 func readConsentFile(path string) (consentFile, error) {
 	var consent consentFile
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return &consent, nil
-	}
 	_, err := toml.DecodeFile(path, &consent)
 	slog.Debug("Read consent file", "file", path, "consent", consent.ConsentState)
 
@@ -181,7 +111,7 @@ func (cf consentFile) write(path string) (err error) {
 	}
 	defer func() {
 		_ = tmp.Close()
-		if err := os.Remove(tmp.Name()); err != nil {
+		if err := os.Remove(tmp.Name()); err != nil && !os.IsNotExist(err) {
 			slog.Warn("Failed to remove temporary file when writing consent file", "file", tmp.Name(), "error", err)
 		}
 	}()
