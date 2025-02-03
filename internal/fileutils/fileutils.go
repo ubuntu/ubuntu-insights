9f1edd72fbcc7c938d5e0d172c6fa0020f7fb5a6
trevor-shoe <trevor.schupbach@canonical.com>
2025-02-03 17:20:50 -0500

    Add fileutil tests
    specifically ConvertUnitToBytes and ReadFileLogError

--- internal/fileutils/fileutils.go
+++ internal/fileutils/fileutils.go
@@ -9,6 +9,35 @@ import (
 	"strings"
 )
 
+// AtomicWrite writes data to a file atomically.
+// If the file already exists, then it will be overwritten.
+// Not atomic on Windows.
+func AtomicWrite(path string, data []byte) error {
+	tmp, err := os.CreateTemp(filepath.Dir(path), "tmp-*.tmp")
+	if err != nil {
+		return fmt.Errorf("could not create temporary file: %v", err)
+	}
+	defer func() {
+		_ = tmp.Close()
+		if err := os.Remove(tmp.Name()); err != nil && !os.IsNotExist(err) {
+			slog.Warn("Failed to remove temporary file", "file", tmp.Name(), "error", err)
+		}
+	}()
+
+	if _, err := tmp.Write(data); err != nil {
+		return fmt.Errorf("could not write to temporary file: %v", err)
+	}
+
+	if err := tmp.Close(); err != nil {
+		return fmt.Errorf("could not close temporary file: %v", err)
+	}
+
+	if err := os.Rename(tmp.Name(), path); err != nil {
+		return fmt.Errorf("could not rename temporary file: %v", err)
+	}
+	return nil
+}
+
 // ReadFileLogError returns the data in the file path, trimming whitespace, or "" on error.
 func ReadFileLogError(path string, log *slog.Logger) string {
 	f, err := os.ReadFile(path)
@@ -55,33 +84,4 @@ func ConvertUnitToBytes(unit string, value int) (int, error) {
 	default:
 		return value, fmt.Errorf("unrecognized bytes unit: %s", unit)
 	}
-}
-
-// AtomicWrite writes data to a file atomically.
-// If the file already exists, then it will be overwritten.
-// Not atomic on Windows.
-func AtomicWrite(path string, data []byte) error {
-	tmp, err := os.CreateTemp(filepath.Dir(path), "tmp-*.tmp")
-	if err != nil {
-		return fmt.Errorf("could not create temporary file: %v", err)
-	}
-	defer func() {
-		_ = tmp.Close()
-		if err := os.Remove(tmp.Name()); err != nil && !os.IsNotExist(err) {
-			slog.Warn("Failed to remove temporary file", "file", tmp.Name(), "error", err)
-		}
-	}()
-
-	if _, err := tmp.Write(data); err != nil {
-		return fmt.Errorf("could not write to temporary file: %v", err)
-	}
-
-	if err := tmp.Close(); err != nil {
-		return fmt.Errorf("could not close temporary file: %v", err)
-	}
-
-	if err := os.Rename(tmp.Name(), path); err != nil {
-		return fmt.Errorf("could not rename temporary file: %v", err)
-	}
-	return nil
 }
