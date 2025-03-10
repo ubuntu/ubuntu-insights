5f2777b981428d721e19834a5068fb48c71f7d6b
Scott Harder <scott.harder@canonical.com>
2025-03-10 09:55:54 -0500

    [server] initialize ingest server

--- cmd/ingest-server/main.go
+++ cmd/ingest-server/main.go
@@ -1,6 +1,51 @@
-// Package main for the ingest-server.
 package main
 
-func main() {
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/ubuntu/ubuntu-insights/cmd/ingest-server/server"
+)
+
+func main() {
+	var cfgPath string
+	flag.StringVar(&cfgPath, "config", "config.json", "Path to configuration file")
+	flag.Parse()
+
+	configManager := server.NewConfigManager(cfgPath)
+	if err := configManager.Load(); err != nil {
+		slog.Error("Failed to load configuration", "err", err)
+		return
+	}
+	go configManager.Watch()
+
+	s := server.NewServer(configManager)
+
+	mux := http.NewServeMux()
+	mux.Handle("POST /upload/{app}", http.HandlerFunc(s.UploadHandler))
+
+	srv := &http.Server{
+		Addr: ":8080",
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler: http.TimeoutHandler(mux, 1 * time.Second, ""),
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	slog.Info("Server starting...")
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "err", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	<-sigChan
+	slog.Info("Shutting down server...")
 }
---
+++ cmd/ingest-server/server/config.go
@@ -0,0 +1,104 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+type Config struct {
+	BaseDir string `json:"base_dir"`
+}
+
+type ConfigManager struct {
+	config Config
+	lock sync.RWMutex
+	configPath string
+}
+
+func NewConfigManager(path string) *ConfigManager {
+	return &ConfigManager{configPath: path}
+}
+
+var (
+	config     Config
+	lock sync.RWMutex
+	configPath = "config.json"
+)
+
+func (cm *ConfigManager) Load() error {
+	file, err := os.Open(cm.configPath)
+	if err != nil {
+		return fmt.Errorf("opening config file: %w", err)
+	}
+	defer file.Close()
+
+	var newConfig Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&newConfig); err != nil {
+		return fmt.Errorf("decoding config JSON: %w", err)
+	}
+
+	cm.lock.Lock()
+	cm.config = newConfig
+	cm.lock.Unlock()
+
+	slog.Info("Configuration loaded", "config", config)
+	return nil
+}
+
+// func WatchConfig() {
+func (cm *ConfigManager) Watch() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		slog.Warn("Failed to create watcher", "err", err)
+		return
+	}
+	defer watcher.Close()
+
+	configDir, _ := filepath.Split(cm.configPath)
+	if configDir == "" {
+		configDir = "."
+
+	}
+	if err := watcher.Add(configDir); err != nil {
+		slog.Warn("Failed to add directory to watcher", "dir", configDir, "err", err)
+		return
+	}
+
+	slog.Info("Watching configuration directory", "dir", configDir)
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op != fsnotify.Write && event.Op != fsnotify.Create {
+				continue
+			}
+
+			slog.Debug("Configuration file changed. Reloading...")
+
+			if err := cm.Load(); err != nil {
+				slog.Warn("Error reloading config", "err", err)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			slog.Warn("Watcher error", "err", err)
+		}
+	}
+}
+
+func (cm *ConfigManager) GetBaseDir() string {
+	cm.lock.RLock()
+	defer cm.lock.RUnlock()
+	return cm.config.BaseDir
+}
