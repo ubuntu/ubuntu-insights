7304c165bcedb9ad22f7e86a661be1236ad81878
trevor-shoe <trevor.schupbach@canonical.com>
2025-03-14 13:28:17 -0400

    [API] Simplify lib generation

--- pkg/insights/C/generate.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo generating Windows shared library...
-:: For this to work you need gcc on Windows. Also you need to run `$env:CGO_ENABLED="1"` to make sure Go does something.
-
-go build -o ../../../build/libinsights.dll.1 -buildmode=c-shared libinsights.go
-move ../../../build/libinsights.dll.h ../../../build/libinsights.h
--- pkg/insights/C/generate_windows.go
+++ pkg/insights/C/generate_windows.go
@@ -1,4 +1,5 @@
 package main
 
+// Make sure cgo is enabled `$env:CGO_ENABLED="1"`.
 // generate shared library and header, this requires setting up a gcc compiler on windows.
-//go:generate cmd.exe /C generate.bat
+//go:generate go build -o ../../../build/libinsights.dll -buildmode=c-shared libinsights.go
