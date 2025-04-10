c5d93aed0e74ddc6587d7cbaabddc2f2d6d6f066
kat <28567881+hk21702@users.noreply.github.com>
2025-04-09 20:27:49 -0400

    Improved testing and memory management

---
+++ internal/collector/sysinfo/hardware/wayland_displays_linux_test.h
@@ -0,0 +1,10 @@
+#ifndef WAYLAND_DISPLAYS_TEST_H
+#define WAYLAND_DISPLAYS_TEST_H
+
+// Setter function to set displays for testing purposes
+void set_displays(struct wayland_display **displays, int count);
+
+// Setter function to set memory error for testing purposes
+void set_memory_error(bool error);
+
+#endif // WAYLAND_DISPLAYS_TEST_H
