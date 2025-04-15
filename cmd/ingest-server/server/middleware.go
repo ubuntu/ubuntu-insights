a8e4fcd538d7da92dd12a61c545668966e8f9f2b
Scott Harder <scott.harder@canonical.com>
2025-04-14 19:22:33 -0500

    [middleware] apply rate and burst to ip limiter

--- cmd/ingest-server/server/middleware.go
+++ cmd/ingest-server/server/middleware.go
@@ -1,29 +1,48 @@
 package server
 
 import (
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
 
 type ipLimiter struct {
+	limiters map[string]*rate.Limiter
+	mu sync.Mutex
+	rate rate.Limit
+	burst int
 }
 
 func newIPLimiter(r rate.Limit, b int) *ipLimiter {
-	return &ipLimiter{}
+	return &ipLimiter{
+		limiters: make(map[string]*rate.Limiter),
+		rate: r,
+		burst: b,
+	}
 }
 
 func (l *ipLimiter) getLimiter(ip string) *rate.Limiter {
-	return nil
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	limiter, exists := l.limiters[ip]
+	if !exists {
+		limiter = rate.NewLimiter(l.rate, l.burst)
+		l.limiters[ip] = limiter
+	}
+	return limiter
 }
 
 func (h Server) rateLimitMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip := r.RemoteAddr
-
-		limiter := h.ipLimiter.getLimiter(ip)
-
-		if !limiter.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Unable to determine IP", http.StatusBadRequest)
+			return
+		}
+		if !h.ipLimiter.getLimiter(ip).Allow() {
 			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
 			return
 		}
