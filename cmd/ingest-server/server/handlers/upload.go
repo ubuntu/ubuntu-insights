df161ede7492e8ecd5eed124444e5039bb4ffc58
Scott Harder <scott.harder@canonical.com>
2025-04-16 20:56:11 -0500

    [handler] add internal logging

--- cmd/ingest-server/server/handlers/upload.go
+++ cmd/ingest-server/server/handlers/upload.go
@@ -18,7 +18,10 @@ const maxUploadSize = 100 << 10 // 100 KB
 var ConfigManager *config.ConfigManager
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	reqID := uuid.New().String()
 	app := r.PathValue("app")
+
+	slog.Info("Request recv'd", "req_id", reqID, "app", app)
 
 	if len(app) < 1 {
 		http.Error(w, "Invalid application name in URL", http.StatusBadRequest)
@@ -37,12 +40,14 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	if !allowed {
 		http.Error(w, "Invalid application name in URL", http.StatusBadRequest)
+		slog.Error("Invalid application name in URL", "req_id", reqID, "app", app)
 		return
 	}
 
 	jsonFile, header, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Error retrieving the file: "+err.Error(), http.StatusBadRequest)
+		slog.Error("Error retrieving the file", "req_id", reqID, "app", app, "err", err)
 		return
 	}
 	defer jsonFile.Close()
@@ -50,11 +55,13 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	jsonData, err := io.ReadAll(jsonFile)
 	if err != nil {
 		http.Error(w, "Error reading the file: "+err.Error(), http.StatusBadRequest)
+		slog.Error("Error reading the file", "req_id", reqID, "app", app, "err", err)
 		return
 	}
 
 	if header.Size > maxUploadSize {
 		http.Error(w, "File exceeds size limit", http.StatusRequestEntityTooLarge)
+		slog.Error("File exceeds size limit", "req_id", reqID, "app", app, "size", header.Size)
 		return
 	}
 
@@ -65,17 +72,19 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	targetDir := filepath.Join(baseDir, app)
 	if err := os.MkdirAll(targetDir, os.ModePerm); err != nil {
 		http.Error(w, "Error creating directory: "+err.Error(), http.StatusInternalServerError)
+		slog.Error("Error creating directory", "req_id", reqID, "app", app, "target", targetDir, "err", err)
 		return
 	}
 
-	safeFilename := fmt.Sprintf("%s.json", uuid.New().String())
+	safeFilename := fmt.Sprintf("%s.json", reqID)
 	targetPath := filepath.Join(targetDir, safeFilename)
 
 	if err := fileutils.AtomicWrite(targetPath, jsonData); err != nil {
 		http.Error(w, "Error saving file: "+err.Error(), http.StatusInternalServerError)
+		slog.Error("Error saving file", "req_id", reqID, "app", app, "target", targetPath, "err", err)
 		return
 	}
 
-	slog.Info("Saved file", "app", app, "target", targetPath)
+	slog.Info("File successfully uploaded", "req_id", reqID, "app", app, "target", targetPath)
 	w.WriteHeader(http.StatusCreated)
 }
