2af67608e140692aef8ead5874d207e5121ee32c
Scott Harder <scott.harder@canonical.com>
2025-04-16 23:17:30 -0500

    [handler/test] add happy path unit test

---
+++ cmd/ingest-server/server/handlers/upload_test.go
@@ -0,0 +1,93 @@
+package handlers_test
+
+import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ubuntu/ubuntu-insights/cmd/ingest-server/server/handlers"
+)
+
+type mockConfigManager struct {
+	BaseDir     string
+	AllowedList []string
+}
+
+func (m *mockConfigManager) GetBaseDir() string {
+	return m.BaseDir
+}
+
+func (m *mockConfigManager) GetAllowList() []string {
+	return m.AllowedList
+}
+
+func setup(t *testing.T) (*handlers.UploadHandler, *mockConfigManager, func()) {
+	tmpDir, err := os.MkdirTemp("", "upload_test")
+	if err != nil {
+		t.Fatalf("Failed to create temp dir: %v", err)
+	}
+
+	mockConfig := &mockConfigManager{
+		BaseDir:     tmpDir,
+		AllowedList: []string{"testapp"},
+	}
+
+	uploadHandler := &handlers.UploadHandler{
+		Config: mockConfig,
+	}
+
+	cleanup := func() {
+		os.RemoveAll(tmpDir)
+	}
+
+	return uploadHandler, mockConfig, cleanup
+}
+
+func createMultipartRequest(app, filename string, data []byte) (*http.Request, error) {
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	fw, err := w.CreateFormFile("file", filename)
+	if err != nil {
+		return nil, err
+	}
+	_, err = fw.Write(data)
+	if err != nil {
+		return nil, err
+	}
+	w.Close()
+
+	req := httptest.NewRequest("POST", "/upload/"+app, &b)
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.SetPathValue("app", app)
+	return req, nil
+}
+
+func TestUploadHandler_Success(t *testing.T) {
+	handler, mockConfig, cleanup := setup(t)
+	defer cleanup()
+
+	data := []byte(`{"foo": "bar"}`)
+	req, err := createMultipartRequest("testapp", "sample.json", data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusCreated {
+		t.Fatalf("Expected status 201 Created, got %d", rr.Code)
+	}
+
+	files, err := os.ReadDir(filepath.Join(mockConfig.GetBaseDir(), "testapp"))
+	if err != nil {
+		t.Fatal("Expected file to be written but directory read failed:", err)
+	}
+	if len(files) != 1 {
+		t.Fatalf("Expected one file to be written, got %d", len(files))
+	}
+}
