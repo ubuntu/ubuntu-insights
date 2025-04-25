204cb9a6d1d1dbe8a65afa5dc86f6f182c741cc5
Scott Harder <scott.harder@canonical.com>
2025-04-24 23:37:26 -0500

    [main] add main program loop of ingest service

--- cmd/ingest-service/main.go
+++ cmd/ingest-service/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"log/slog"
+	"time"
+
+	"github.com/ubuntu/ubuntu-insights/cmd/ingest-service/config"
+	"github.com/ubuntu/ubuntu-insights/cmd/ingest-service/processor"
+	"github.com/ubuntu/ubuntu-insights/cmd/ingest-service/storage"
 )
 
 func main() {
@@ -13,4 +18,24 @@ func main() {
 		slog.Error("Configuration file path is required")
 		return
 	}
+
+	cfg, err := config.Load(*cfgPath)
+	if err != nil {
+		slog.Error("Failed to load configuration", "err", err)
+		return
+	}
+
+	if err := storage.Initialize(cfg.DB); err != nil {
+		slog.Error("Failed to initialize database connection", "err", err)
+		return
+	}
+
+	for {
+		if err := processor.ProcessFiles(cfg); err != nil {
+			slog.Warn("Failed to process files", "err", err)
+		}
+
+		time.Sleep(30 * time.Second)
+	}
+
 }
