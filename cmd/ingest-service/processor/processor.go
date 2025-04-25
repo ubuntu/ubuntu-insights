c2737d88a5f89783d3376f8711078660df0cd7f1
Scott Harder <scott.harder@canonical.com>
2025-04-24 22:38:20 -0500

    [processor] add helper functions to iterate through dir and process individual files

--- cmd/ingest-service/processor/processor.go
+++ cmd/ingest-service/processor/processor.go
@@ -3,13 +3,32 @@
 package processor
 
 import (
+	"fmt"
+
 	"github.com/ubuntu/ubuntu-insights/cmd/ingest-service/config"
 )
+
+func getJSONFiles(dir string) ([]string, error) {
+	return nil, nil
+}
+
+func processSingleFile(file string) error {
+	return nil
+}
 
 // ProcessFiles processes all JSON files in the specified directory.
 // It reads each file, unmarshals the JSON data into a FileData struct,
 // and uploads the data to a PostgreSQL database.
 // After processing, it removes the file from the filesystem.
 func ProcessFiles(cfg *config.ServiceConfig) error {
+	files, err := getJSONFiles(cfg.InputDir)
+	if err != nil {
+		return fmt.Errorf("failed to get JSON files: %w", err)
+	}
+
+	for _, file := range files {
+		processSingleFile(file)
+	}
+
 	return nil
 }
