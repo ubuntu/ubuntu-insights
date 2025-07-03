9012714672a60a195c06aa7b449b81cead7b2fcd
kkuo <kat.kuo@canonical.com>
2025-07-03 18:26:05 -0400

    Add distribution-version index to ubuntu_report table

--- server/migrations/000005_initialize-ubuntu_report.down.sql
+++ server/migrations/000005_initialize-ubuntu_report.down.sql
@@ -1,7 +1,6 @@
 DROP INDEX IF EXISTS idx_ubuntu_report_entry_time_optout;
-
+DROP INDEX IF EXISTS idx_ubuntu_report_distribution_version;
 DROP INDEX IF EXISTS idx_ubuntu_report_report;
-
 DROP INDEX IF EXISTS idx_ubuntu_report_report_id;
 
 DROP TABLE IF EXISTS ubuntu_report;
