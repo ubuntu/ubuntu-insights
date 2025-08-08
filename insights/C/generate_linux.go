59c7387bbd9441b504624ceabb009885768b1f68
kkuo <kat.kuo@canonical.com>
2025-08-07 23:35:14 -0400

    fix: update output paths for generated libraries and also copy types header file

--- insights/C/generate_linux.go
+++ insights/C/generate_linux.go
@@ -2,4 +2,7 @@
 package main
 
 // generate shared library and header.
-//go:generate go build -o ../../build/libinsights.so -buildmode=c-shared libinsights.go
+//go:generate go build -o ../generated/libinsights.so -buildmode=c-shared libinsights.go
+
+// Copy insights_types.h to the generated folder
+//go:generate cp insights_types.h ../generated/types.h
--- insights/C/generate_windows.go
+++ insights/C/generate_windows.go
@@ -3,4 +3,7 @@ package main
 
 // Make sure cgo is enabled `$env:CGO_ENABLED="1"`.
 // generate shared library and header, this requires setting up a gcc compiler on windows.
-//go:generate go build -o ../../build/libinsights.dll -buildmode=c-shared libinsights.go
+//go:generate go build -o ../generated/libinsights.dll -buildmode=c-shared libinsights.go
+
+// Copy insights_types.h to the generated folder
+//go:generate sh -c "cp insights_types.h ../generated/types.h"
