932c4e25f5c051f5e15468cc4215a5bf0b3ae0d8
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-09-04 23:37:25 +0200

    Appease linter
    equal-values: use require.Equal (testifylint)

--- insights/internal/collector/collector_test.go
+++ insights/internal/collector/collector_test.go
@@ -462,7 +462,7 @@ func TestWrite(t *testing.T) {
 			require.NoError(t, err)
 
 			want := testutils.LoadWithUpdateFromGoldenYAML(t, got)
-			require.EqualValues(t, want, got)
+			require.Equal(t, want, got)
 		})
 	}
 }
