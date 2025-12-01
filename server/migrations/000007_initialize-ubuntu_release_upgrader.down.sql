672fb6e9e27a53454b87d0594b9057850e494828
kkuo <kat.kuo@canonical.com>
2025-12-01 09:23:11 -0500

    Add wsl_setup and ubuntu_release_upgrader sources to migrations

---
+++ server/migrations/000007_initialize-ubuntu_release_upgrader.down.sql
@@ -0,0 +1,9 @@
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_entry_time_optout;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_source_metrics;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_platform;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_software;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_hardware;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_collection_time;
+DROP INDEX IF EXISTS idx_ubuntu_release_upgrader_report_id;
+
+DROP TABLE IF EXISTS ubuntu_release_upgrader;
